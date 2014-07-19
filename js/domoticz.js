9ca51efd7792b645599258df08c6a84862d5a407
Sander Filius <s.filius@me.com>
2014-07-19 20:51:34 +0200

    Fiddling around with the uservariables. Themeswitcher now stores it's settings in domoticz :)

--- js/domoticz.js
+++ js/domoticz.js
@@ -13,8 +13,9 @@
  
  ;(function ( $, window, document, undefined ) {
  
+ // Functions for the API
  
- 	//get all uservariables, print a list to console & return as object
+ 	//get all uservariables return as array
 	 $.getUservariables = function() {
 		var userVariables = [];
 		
@@ -27,15 +28,11 @@
 	  		}
 	  	});
 	  	
-	  	userVariables.result.forEach(function(userVariable,key){
-			console.log(userVariable.idx, userVariable.Name, userVariable.Value, userVariable.Type, userVariable.LastUpdate)
-		});
-
 	return userVariables;
 	}
 
 
-	// get specified uservariable, print it to console & return as object
+	// get specified uservariable, return as array
  	$.getUservariable = function(idx) {
 		var userVariable = [];
 		
@@ -47,13 +44,17 @@
 	  		userVariable = json;
 	  		}
 	  	});
-	
-	  console.log(userVariable.title, idx, userVariable.result[0].Name, userVariable.result[0].Type, userVariable.result[0].Value, userVariable.status);
 	  return userVariable;
 	  }
 	  
-
-	  // try to save a variable, print results to console & return results as object
+	  // try to save a variable & return results as object
+	  /* 	0 = Integer, e.g. -1, 1, 0, 2, 10  
+	  *		1 = Float, e.g. -1.1, 1.2, 3.1
+	  *		2 = String
+	  *		3 = Date in format DD/MM/YYYY
+	  *		4 = Time in 24 hr format HH:MM
+	  */		
+	  
 	  $.saveUservariable = function(vname, vtype, vvalue){
 		  var result = [];
 		
@@ -66,12 +67,11 @@
 	  		}
 	  	});
 
-	  console.log(result.title, vname, vtype, vvalue, result.status);
 	  return result;
 	  }
 	  
 
-	  //try to update a variable, print results to console & return results as object
+	  //try to update a variable & return results as object
 	  $.updateUservariable = function(idx, vname, vtype, vvalue){
 		var result = [];
 	
@@ -84,7 +84,6 @@
 	  		}
 	  	});
 
-	  console.log(result.title, idx, vname, result.status);
 	  return result;
 }
 
@@ -106,4 +105,30 @@
 	  	return result;
 }
 
+// Funtions for the webinterface	
+
+	//return all variables in an easy objects: name = array, name = idx
+	//need to have the names & the idx's arond, want to minimize the for.Eache's, so this seams easy :P
+	getDomoticzVariables = function(){
+		domoticzval = {};
+		domoticzidx = {};
+		
+		var variables = $.getUservariables();
+		if (variables.result != undefined){
+		variables.result.forEach(function(value,key){
+			domoticzval[value.Name] = value.Value;
+			domoticzidx[value.Name] = value.idx;
+			})
+			}
+	
+	//defaults
+	if (!domoticzval.framb0ise_theme){$.saveUservariable("framb0ise_theme", 0, 0); getDomoticzVariables()}
+	
+	return domoticzval;
+	return domoticzidx;
+	}
+   
 }(jQuery, window, document));
+
+// init variables to start :)
+getDomoticzVariables();  
